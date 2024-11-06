dd7f37f3b36bae46626a1c0376cbb94914265409
Trương Công Quốc Cường <59004156+TeoLeoGit@users.noreply.github.com>
2024-11-06 10:29:53 +0700

    Clean project.

--- Game off 2024/Assets/_Scripts/Managers/GameManager.cs
+++ Game off 2024/Assets/_Scripts/Managers/GameManager.cs
@@ -4,29 +4,4 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    [SerializeField] GameObject _prefabLevel;
-    private GameObject _currentLevel;
-
-    private void Awake()
-    {
-        MainController.OnNewGame += InstantiateLevel;
-        MainController.OnReturnHome += DestroyLevel;
-    }
-
-    private void OnDestroy()
-    {
-        MainController.OnNewGame -= InstantiateLevel;
-        MainController.OnReturnHome -= DestroyLevel;
-    }
-
-    void InstantiateLevel()
-    {
-        _currentLevel = Instantiate(_prefabLevel, transform);
-        _currentLevel.transform.position = new Vector3(-5, 2, 0);
-    }
-
-    void DestroyLevel()
-    {
-        Destroy(_currentLevel);
-    }
 }
